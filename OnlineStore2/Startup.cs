68d1b25a7c4211e1ac355b05133f603d05d18cd8
navid khodaveisi <navidkhodaveisi2@gmail.com>
2023-05-20 03:34:40 +0100

    All the setup and coping process is done successfully. The project now displays the Home\Index view.

---
+++ OnlineStore2/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Owin;
+using Owin;
+using OnlineStore2.Data.Repositories;
+using OnlineStore2.Models;
+
+[assembly: OwinStartupAttribute(typeof(OnlineStore2.Startup))]
+namespace OnlineStore2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+    }
+}
